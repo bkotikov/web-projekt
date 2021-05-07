c45e6819a59f41f8b798877a8340e384d6c18dac
n.rudolph <1922602@stud.hs-mannheim.de>
2021-05-07 23:26:06 +0200

    getCookie(cname) für spezifische cookies

--- js/index.js
+++ js/index.js
@@ -2,21 +2,50 @@ function getIndexPage() {
     return new Promise(resolve)
 }
 
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
 
 var body = document.getElementById("body");
 var ok = document.getElementById("ok");
+var deny = document.getElementById("reject");
 var popup = document.getElementById("cookie");
 var overlay = document.getElementById("overlay");
 
-if (document.cookie === "") {
+
+
+
+console.log(getCookie("dvsgo"));
+var dvsgo = getCookie("dvsgo");
+if (dvsgo !== "accecpted" && dvsgo !== "rejected") {
+    console.log("jo")
     body.style.overflow = "hidden";
     popup.style.display = "block";
     overlay.style.display = "block";
 }
  
 ok.addEventListener('click', () => {
+    document.cookie = "dvsgo=accecpted; path=/; samesite=strict";
     body.style.overflow = "auto";
     popup.style.display = "none";
     overlay.style.display = "none";
-    document.cookie = "cookie=accecpted; path=/; samesite=strict";
-}, true)
+}, true);
+
+deny.addEventListener('click', () => {
+    document.cookie = "dvsgo=rejected; path=/; samesite=strict";
+    body.style.overflow = "auto";
+    popup.style.display = "none";
+    overlay.style.display = "none";
+}, true);
