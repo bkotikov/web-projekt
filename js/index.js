4cdecf97de118c83fd7eb4942cc73709978170c6
n.rudolph <1922602@stud.hs-mannheim.de>
2021-05-13 00:50:06 +0200

    banner auch ausgelagert und alle verlinkungen

--- js/index.js
+++ js/index.js
@@ -2,6 +2,13 @@ function getIndexPage() {
     return new Promise(resolve)
 }
 
+
+
 $(document).ready(function(){
-  $("#content").load("/de/cookie");
+  var first = $(location).attr('pathname');
+  first.indexOf(1);
+  first.toLowerCase();
+  first = first.split("/")[1];
+  
+  $("#content").load("/" + first + "/cookie");
 });
