3e697071ee16d0ba4f35806034f2072c9c770fab
n.rudolph <1922602@stud.hs-mannheim.de>
2021-05-17 17:48:44 +0200

    login register besser. ru files renamed

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const path    = require("path");
 const parser = require('accept-language-parser');
 const url = require('url');
 
-const PORT = 3000;
+const PORT = 5000;
 
 //Sprachen, die im URL eingesetzt werden
 const en = 'en';
@@ -82,7 +82,7 @@ app.get('/de',function(req,res){
   res.sendFile(path.join(__dirname+'/html/de/index.html'));
 });
 app.get('/ru',function(req,res){
-  res.sendFile(path.join(__dirname+'/html/ru/RU_index.html'));
+  res.sendFile(path.join(__dirname+'/html/ru/index.html'));
 });
 app.get('/bgr',function(req,res){
   res.sendFile(path.join(__dirname+'/html/bgr/index.html'));
@@ -100,7 +100,7 @@ app.get('/en/login', function(req,res) {
   res.sendFile(path.join(__dirname + '/html/en/login.html'));
 });
 app.get('/ru/login', function(req,res) {
-  res.sendFile(path.join(__dirname + '/html/ru/RU_login.html'));
+  res.sendFile(path.join(__dirname + '/html/ru/login.html'));
 });
 app.get('/bgr/login', function(req,res) {
   res.sendFile(path.join(__dirname + '/html/bgr/login.html'));
@@ -119,7 +119,7 @@ app.get('/de/registration', function(req, res){
   res.sendFile(path.join(__dirname + '/html/de/registration.html'))
 });
 app.get('/ru/registration', function(req, res){
-  res.sendFile(path.join(__dirname + '/html/ru/RU_registration.html'))
+  res.sendFile(path.join(__dirname + '/html/ru/registration.html'))
 });
 app.get('/en/registration', function(req, res){
   res.sendFile(path.join(__dirname + '/html/en/registration.html'))
@@ -137,7 +137,7 @@ app.get('/de/about', function(req, res){
   res.sendFile(path.join(__dirname + '/html/de/about.html'))
 });
 app.get('/ru/about', function(req, res){
-  res.sendFile(path.join(__dirname + '/html/ru/RU_about.html'))
+  res.sendFile(path.join(__dirname + '/html/ru/about.html'))
 });
 app.get('/en/about', function(req, res){
   res.sendFile(path.join(__dirname + '/html/en/about.html'))
@@ -153,7 +153,7 @@ app.get('/de/privacy', function(req, res){
   res.sendFile(path.join(__dirname + '/html/de/privacy.html'))
 });
 app.get('/ru/privacy', function(req, res){
-  res.sendFile(path.join(__dirname + '/html/ru/RU_privacy.html'))
+  res.sendFile(path.join(__dirname + '/html/ru/privacy.html'))
 });
 app.get('/en/privacy', function(req, res){
   res.sendFile(path.join(__dirname + '/html/en/privacy.html'))
@@ -170,7 +170,7 @@ app.get('/de/imprint', function(req, res){
   res.sendFile(path.join(__dirname + '/html/de/imprint.html'))
 });
 app.get('/ru/imprint', function(req, res){
-  res.sendFile(path.join(__dirname + '/html/ru/RU_imprint.html'))
+  res.sendFile(path.join(__dirname + '/html/ru/imprint.html'))
 });
 app.get('/en/imprint', function(req, res){
   res.sendFile(path.join(__dirname + '/html/en/imprint.html'))
@@ -181,6 +181,25 @@ app.get('/bgr/imprint', function(req, res){
 
 //-----Imprint----
 
+
+
+//-----GEZ-----
+
+app.get('/de/gez', function(req, res){
+  res.sendFile(path.join(__dirname + '/html/de/gez.html'))
+});
+app.get('/ru/gez', function(req, res){
+  res.sendFile(path.join(__dirname + '/html/ru/gez.html'))
+});
+app.get('/en/gez', function(req, res){
+  res.sendFile(path.join(__dirname + '/html/en/gez.html'))
+});
+app.get('/bgr/gez', function(req, res){
+  res.sendFile(path.join(__dirname + '/html/bgr/gez.html'))
+});
+
+//-----GEZ-----
+
 app.post('/de/loginPruefen', function(req,res){
   res.sendFile(path.join(__dirname+'/html/de/index.html'));
 });
@@ -188,11 +207,14 @@ app.post('/en/loginPruefen', function(req,res){
   res.sendFile(path.join(__dirname+'/html/en/index.html'));
 });
 app.post('/ru/loginPruefen', function(req,res){
-  res.sendFile(path.join(__dirname+'/html/ru/RU_index.html'));
+  res.sendFile(path.join(__dirname+'/html/ru/index.html'));
 });
 app.post('/bgr/loginPruefen', function(req,res){
   res.sendFile(path.join(__dirname+'/html/bgr/index.html'));
 });
+
+
+
 
 
 
@@ -203,7 +225,7 @@ app.post('/en/registrierungPruefen', function(req,res){
   res.sendFile(path.join(__dirname+'/html/en/index.html'));
 });
 app.post('/ru/registrierungPruefen', function(req,res){
-  res.sendFile(path.join(__dirname+'/html/ru/RU_index.html'));
+  res.sendFile(path.join(__dirname+'/html/ru/index.html'));
 });
 app.post('/bgr/registrierungPruefen', function(req,res){
   res.sendFile(path.join(__dirname+'/html/bgr/index.html'));
--- js/validierung.js
+++ js/validierung.js
@@ -1,17 +1,23 @@
-document.getElementById("emailAdresse").onchange = function() {validiereEmail(this.value)};
-document.getElementById("passwort").onchange = function(){validierePasswort(this.value)};
+const email = document.getElementById("emailAdresse");
+const pass = document.getElementById("passwort");
+
+const emailFehler = document.getElementById("emailFehlermeldung");
+const passFehler = document.getElementById("passwortFehlermeldung");
+
+email.oninput = function() {validiereEmail(this.value)};
+pass.oninput = function(){validierePasswort(this.value)};
 var fehler = false;
 function validiereEmail(email){
   //var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   var mailformat = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
   if (!email.trim().match(mailformat)){
-    document.getElementById("emailFehlermeldung").style.display = "block";
+    emailFehler.style.display = "block";
     if (fehler !== true) {
       fehler = true;
     }
   }else if (email.trim().match(mailformat)){
     if (fehler !== false) fehler =false;
-    document.getElementById("emailFehlermeldung").style.display = "none";
+    emailFehler.style.display = "none";
   }
 }
 var passwortFehler = false;
@@ -19,30 +25,51 @@ function validierePasswort(value){
   var passwortRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,}$/;
 
   if (!value.trim().match(passwortRegex)){
-    document.getElementById("passwortFehlermeldung").style.display = "block";
+    passFehler.style.display = "block";
     if (passwortFehler !== true) {
       passwortFehler = true;
     }
   } else if (value.trim().match(passwortRegex)){
     if (passwortFehler !== false) {passwortFehler =false;}
-    document.getElementById("passwortFehlermeldung").style.display = "none";
+    passFehler.style.display = "none";
   }
 }
 
 var plz = document.getElementById("postleitzahl");
 if (plz){
-  plz.onchange = function () {validierePlz(this.value)};
+  plz.oninput = function () {validierePlz(this.value)};
 }
 
 var plzFehler = false;
+var plzFehlerEl = document.getElementById("plzFehlermeldung");
 function validierePlz(value) {
   var plzFormat = /^[0-9]{5}$/;
   if(!value.trim().match(plzFormat) && value.length > 0){
-    document.getElementById("plzFehlermeldung").style.display = "block";
+    plzFehlerEl.style.display = "block";
     if(plzFehler !== true)plzFehler = true;
   } else if (value.trim().match(plzFormat)){
-    document.getElementById("plzFehlermeldung").style.display = "none";
+    plzFehlerEl.style.display = "none";
     if (plzFehler !== false)plzFehler = false;
+  }
+}
+
+var hFehler = false;
+var hausnummer = document.getElementById("hausnummer");
+var hNummerFehler = document.getElementById("hnumberFehlermeldung");
+if (hausnummer) {
+  hausnummer.oninput = function() {validiereHausNummer(this.value)};
+}
+
+
+function validiereHausNummer(value) {
+  
+  var hausnummerFormat = /^[0-9]+$/;
+  if(!value.trim().match(hausnummerFormat) && value.length > 0){
+    hNummerFehler.style.display = "block";
+    if(hFehler !== true)hFehler = true;
+  } else if (value.trim().match(hausnummerFormat)){
+    hNummerFehler.style.display = "none";
+    if (hFehler !== false)hFehler = false;
   }
 }
 
