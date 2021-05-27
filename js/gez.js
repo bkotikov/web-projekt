45eb7887907a84063fa124e7aaadfa2f40428658
bkotikov <boggikotikov@gmail.com>
2021-05-27 18:31:58 +0300

    Formular

--- js/gez.js
+++ js/gez.js
@@ -1,45 +1,41 @@
+var queryString = window.location.search;
+var urlParams = new URLSearchParams(queryString);
 var allPages = document.getElementsByClassName("page");
-for (let index = 0; index < allPages.length; index++) {
-  allPages[index].style.display = "none";
+
+function setRightUrlParams() {
+  if (parseInt(urlParams.get("page")) > allPages.length - 1) {
+    urlParams.set("page", allPages.length - 1);
+    window.history.pushState(this.href, '', `${location.pathname}?${urlParams}`);
+  } else if (parseInt(urlParams.get("page")) < 0) {
+    urlParams.set("page", 0);
+    window.history.pushState(this.href, '', `${location.pathname}?${urlParams}`);
+  } else {
+    console.log("Position:" + parseInt(urlParams.get("page")));
+  }
 }
 
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-console.log((urlParams.has('page')));
-console.log((urlParams.get("page")));
-if (!(urlParams.has("page"))) {
-  urlParams.set("page", 0);
-  window.history.pushState(this.href, '', `${location.pathname}?${urlParams}`);
+setRightUrlParams();
+
+function getAndSetUrlParams() {
+  for (let index = 0; index < allPages.length; index++) {
+    allPages[index].style.display = "none";
+  }
+  allPages[parseInt(urlParams.get("page"))].style.display = "block";
+
+  console.log("accessed getAndSetUrlParams Function");
 }
 
-if (parseInt(urlParams.get("page")) > allPages.length - 1) {
-  urlParams.set("page", allPages.length - 1);
-  window.history.pushState(this.href, '', `${location.pathname}?${urlParams}`);
-} else if (parseInt(urlParams.get("page")) < 0) {
-  urlParams.set("page", 0);
-  window.history.pushState(this.href, '', `${location.pathname}?${urlParams}`);
-} else {
-  console.log("Position:" + parseInt(urlParams.get("page")));
-}
+getAndSetUrlParams();
 
-allPages[parseInt(urlParams.get("page"))].style.display = "block";
 
-const successdiv = document.getElementById("success-div");
-successdiv.style.display = "none";
 const next = document.getElementById("next");
 next.addEventListener("click", function () {
+  console.log("position at next: " + urlParams.get("page") + 1);
   if ((parseInt(urlParams.get("page")) + 1) == allPages.length) {
     next.setAttribute('type', 'submit');
-    console.log("submitted");
-    successdiv.style.display = "block";
-    setTimeout(() => {
-      console.log("Submitted");
-      successdiv.style.display = "none";
-    }, 3000);
   } else {
-    urlParams.set("page", parseInt(urlParams.get("page")) + 1);
-    window.history.replaceState({ page: this.href }, '', `${location.pathname}?${urlParams}`);
-    window.top.location = window.top.location;
+    replaceUrlParams(true);
+    hidePages();
   }
 });
 
@@ -50,8 +46,46 @@ prev.addEventListener("click", function () {
     console.log("prev");
     setTimeout(() => { console.log("World!"); }, 5000);
   } else {
-    urlParams.set("page", parseInt(urlParams.get("page")) - 1);
-    window.history.replaceState({ page: this.href }, '', `${location.pathname}?${urlParams}`);
-    window.top.location = window.top.location;
+    replaceUrlParams(false);
+    hidePages();
   }
 });
+
+function hidePages() {
+  for (let index = 0; index < allPages.length; index++) {
+    allPages[index].style.display = "none";
+  }
+  allPages[parseInt(urlParams.get("page"))].style.display = "block";
+}
+
+function savePositionInCookie(params) {
+
+}
+
+function overwriteCookieIfExist(params) {
+
+}
+
+function getCookieIfExist(params) {
+
+}
+
+function replaceUrlParams(nextOrPrev) {
+  if (nextOrPrev) {
+    urlParams.set("page", parseInt(urlParams.get("page")) + 1);
+    window.history.replaceState({ page: this.href }, '', `${location.pathname}?${urlParams}`);
+  } else {
+    urlParams.set("page", parseInt(urlParams.get("page")) - 1);
+    window.history.replaceState({ page: this.href }, '', `${location.pathname}?${urlParams}`);
+  }
+}
+
+function reloadPage() {
+  const urlParams = new URLSearchParams(queryString);
+  if (!(urlParams.has("page"))) {
+    urlParams.set("page", 0);
+    window.history.pushState(this.href, '', `${location.pathname}?${urlParams}`);
+  }
+}
+
+reloadPage();
