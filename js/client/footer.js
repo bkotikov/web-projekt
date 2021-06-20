ab2f9952fede479cb98ca2c96fa799d5c7655ac3
n.rudolph <1922602@stud.hs-mannheim.de>
2021-06-20 17:20:38 +0200

    sprachauswahl im footer

--- js/client/footer.js
+++ js/client/footer.js
@@ -4,6 +4,36 @@ const ru = document.getElementById("ru");
 const bg = document.getElementById("bg");
 
 
+de.addEventListener("click", changeLangDe);
+en.addEventListener("click", changeLangEn);
+ru.addEventListener("click", changeLangRu);
+bg.addEventListener("click", changeLangBg);
+
+function changeLangDe() {
+    changeLang("de");
+}
+function changeLangEn() {
+    changeLang("en");
+}
+function changeLangRu() {
+    changeLang("ru");
+}
+function changeLangBg() {
+    changeLang("bg");
+}
+
+
+
 function changeLang(lang) {
-    window.location.pathname.split('/')[1]
+    if (lang === window.location.pathname.split('/')[1]) {
+        return;
+    }
+    path = window.location.pathname.split('/');
+    newPath = "/" + lang;
+    for (let index = 2; index < path.length; index++) {
+       const element = path[index];
+       newPath += "/" + element;
+    }
+   
+   window.location.href = newPath + window.location.hash;
 }
