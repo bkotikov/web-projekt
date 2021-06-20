4eb59809fbbe2b8c5a38b5be8ab17615d6e08d03
n.rudolph <1922602@stud.hs-mannheim.de>
2021-06-20 16:39:55 +0200

    footer

---
+++ js/client/footer.js
@@ -0,0 +1,9 @@
+const de = document.getElementById("de");
+const en = document.getElementById("en");
+const ru = document.getElementById("ru");
+const bg = document.getElementById("bg");
+
+
+function changeLang(lang) {
+    
+}
